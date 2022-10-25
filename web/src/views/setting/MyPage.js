ad91fb66aa4336a7c88d1225f46c9a783614564e
김재홍 <jhkim@onthelive.kr>
2022-10-25 14:57:28 +0900

    설정 - 마이페이지 추가

---
+++ web/src/views/setting/MyPage.js
@@ -0,0 +1,19 @@
+import {withRouter} from "react-router-dom";
+import {withSnackbar} from "notistack";
+import {withStyles} from "@material-ui/core/styles";
+import {inject} from "mobx-react";
+
+const styles = theme => ({
+
+})
+
+
+class MyPage extends React.Component {
+
+}
+
+export default withSnackbar(withRouter((withStyles(styles) (
+    inject('userStore')
+    (MyPage)
+    )
+)));
