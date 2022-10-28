276e8e9e5b76e5682d6f5c55bb8ab3bd44fa19a5
김재홍 <jhkim@onthelive.kr>
2022-10-28 11:11:26 +0900

    @Mapper 의존성 주입

--- src/main/java/io/aetherit/project/base/repository/mapper/UserMapper.java
+++ src/main/java/io/aetherit/project/base/repository/mapper/UserMapper.java
@@ -2,9 +2,12 @@ package io.aetherit.project.base.repository.mapper;
 
 import io.aetherit.project.base.model.BaseUser;
 import io.aetherit.project.base.model.support.BaseUserType;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
+
+@Mapper
 public interface UserMapper {
     BaseUser selectUser(String id);
     List<BaseUser> selectUsersWhereType(BaseUserType type);
