976d854f1fc21f5095a48afd7be2361700913a8b
김재홍 <jhkim@onthelive.kr>
2022-11-02 19:19:10 +0900

    [********** 중간저장]

--- src/main/java/io/aetherit/project/base/controller/InformationController.java
+++ src/main/java/io/aetherit/project/base/controller/InformationController.java
@@ -13,9 +13,9 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/api/v1/informations")
 public class InformationController {
     private static final Logger logger = LoggerFactory.getLogger(InformationController.class);
-    public static final String NAME = "kr.onthelive.jhkim.localtest";
+    public static final String NAME = "io.aetherit.project.base";
     public static final String VERSION = "1.0-SNAPSHOT";
-    public static final String BUILD_ID = "20221024_1538";
+    public static final String BUILD_ID = "20221102_1916";
     public static final String BUILD_NUMBER = "0";
 
     public InformationController() {
