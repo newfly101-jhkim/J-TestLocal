5dad7df58e14a724c99f48d20bff26ce1a994e24
bckim-onthelive <dntldn@naver.com>
2022-11-03 16:44:51 +0900

    (주석)마이페이지 정보수정 1차 프로토타입 Store로 이관 완료

---
+++ web/src/testtech/AuthStorePrac3.js
@@ -0,0 +1,193 @@
+import {makeAutoObservable} from "mobx";
+import {AuthTokenStorageKey} from "../repositories/Repository";
+
+export const State = {
+    Authenticated: 'Authenticated',
+    NotAuthenticated: 'NotAuthenticated',
+    Pending: 'Pending',
+    Failed: 'Failed',
+};
+
+export const LocalStorageTokenKey = '_BASKITOP_AUTHENTICATION_TOKEN_';
+export const LocalStorageUserId = '_BASKITOP_AUTHENTICATION_USERID_';
+
+const EmptyLogin = {
+    id: '',
+    password: '',
+};
+
+const EmptyUser = {
+    id: '',
+    name: '',
+    type: '',
+    createdDatetime: '',
+    updatedDatetime: '',
+};
+
+const AuthState = {
+    None: 'None',
+    Authenticating: 'Authenticating',
+    Authenticated: 'Authenticated',
+    Error: 'Error',
+}
+const LoginState = {
+    AuthenticationFailed : 'AuthenticationFailed',
+    AuthenticationEnabledError : 'AuthenticationEnabledError',
+    AuthenticationPasswordError : 'AuthenticationPasswordError',
+    AuthenticationUserIdError : 'AuthenticationUserIdError',
+}
+
+// const LogPrefix = '[AuthStore] ';
+
+export default class AuthStore {
+    constructor(props) {
+        this.authRepository = props.authRepository;
+
+        this.authState = AuthState.None;
+
+        makeAutoObservable(this);
+    }
+
+    login = Object.assign({}, EmptyLogin);
+    loginState = State.NotAuthenticated;
+    loginToken = '';
+    loginUser = Object.assign({}, EmptyUser);
+    loginUserState = null;
+    isCheckedUserId = false;
+    realUpdateTime = 0;
+    renameSwitch = true;
+    realStatus = '';
+    realBoolean = '';
+
+    // startChangeInfo = () => {
+    //     this.realUpdateTime = this.loginUser.updatedDatetime;
+    // }
+
+    convertStatus = () => {
+        this.realBoolean = this.loginUser.enabled ? "활성화" : "비활성화";
+        this.realStatus = this.realBoolean
+    }
+
+    handleOnChangeId = (changeId) => {
+        this.loginUser.id = changeId;
+    }
+
+    handleOnChangeName = (changeName) => {
+        this.loginUser.name = changeName;
+    }
+
+    // handleOnChangeStatus = (changeStatus) => {
+    //     this.loginUser.enabled = changeStatus === "활성화" ? true : false;
+    //     // this.loginUser.enabled = changeStatus;
+    //     // this.loginUser.enabled ? "활성화" : "비활성화";
+    // }
+    // handleOnChangeStatus = (changeStatus) => {
+    //     this.loginUser.enabled = changeStatus.target.value === "활성화" ? true : false;
+    //     // this.loginUser.enabled = changeStatus;
+    //     // this.loginUser.enabled ? "활성화" : "비활성화";
+    // }
+    handleOnChangeStatus = (changeStatus) => {
+        this.realStatus = changeStatus.target.value;
+        this.loginUser.enabled = this.realStatus === "활성화" ? true : false
+        // this.loginUser.enabled = changeStatus;
+        // this.loginUser.enabled ? "활성화" : "비활성화";
+    }
+
+    CanChangeInfo = () => {
+        this.renameSwitch = false
+    }
+
+    EndChangeInfo = () => {
+        let nowTime = Date();
+        this.renameSwitch = true;
+        this.loginUser.updatedDatetime = nowTime;
+    }
+
+    changeLoginId = (id) => {
+        this.login.id = id;
+    };
+
+    changeLoginPassword = (password) => {
+        this.login.password = password;
+    };
+
+    invalidateLogin = () => {
+        this.login = Object.assign({}, EmptyLogin);
+        this.loginState = State.NotAuthenticated;
+        this.loginUser = Object.assign({}, EmptyUser);
+        this.loginUserState = null;
+    };
+
+    handleIsCheckedUserId = (check) => {
+        this.isCheckedUserId = check;
+    };
+
+    *doLogin(callbacks) {
+        this.loginState = State.Pending;
+
+        try {
+            const param = this.login;
+            const response = yield this.authRepository.signIn(param);
+            const token = response.token;
+            const user = response.user;
+
+            if(this.isCheckedUserId) {
+                localStorage.setItem(LocalStorageUserId, this.login.id);
+            } else {
+                localStorage.removeItem(LocalStorageUserId);
+            }
+            callbacks.moveTo();
+            console.log(this);
+
+            this.loginState = State.Authenticated;
+            this.loginUserState = null;
+            this.loginToken = token;
+            this.loginUser = user;
+        } catch (e) {
+            console.log(e.response);
+            let errorCode = e.response.data.errorCode;
+            if (errorCode === LoginState.AuthenticationEnabledError) {
+                this.loginUserState = "사용 불가능한 계정입니다.";
+            } else if (errorCode === LoginState.AuthenticationUserIdError) {
+                this.loginUserState = "등록되지 않은 계정입니다.";
+            } else if (errorCode === LoginState.AuthenticationPasswordError) {
+                this.loginUserState = "계정정보가 일치하지 않습니다.";
+            }
+            this.login.id = localStorage.getItem(LocalStorageUserId);
+            this.login.password = '';
+            this.loginState = State.NotAuthenticated;
+            this.loginUser = Object.assign({}, EmptyUser);
+        }
+    }
+
+    *checkLogin() {
+        const token = sessionStorage.getItem(AuthTokenStorageKey);
+
+        if(token) {
+            try {
+                const user = yield this.authRepository.signCheck();
+                this.loginState = State.Authenticated;
+                this.loginUser = user;
+            } catch(e) {
+                this.login = Object.assign({}, EmptyLogin);
+                this.loginState = State.NotAuthenticated;
+                this.loginUser = Object.assign({}, EmptyUser);
+            }
+        }
+    }
+
+    *doLogout() {
+        sessionStorage.removeItem(AuthTokenStorageKey);
+
+        try {
+            yield this.authRepository.signOut();
+            this.login = Object.assign({}, EmptyLogin);
+            this.loginState = State.NotAuthenticated;
+            this.loginUser = Object.assign({}, EmptyUser);
+        } catch(e) {
+            this.login = Object.assign({}, EmptyLogin);
+            this.loginState = State.NotAuthenticated;
+            this.loginUser = Object.assign({}, EmptyUser);
+        }
+    }
+}
---
+++ web/src/testtech/MyPagePrac3.js
@@ -0,0 +1,249 @@
+import React from "react";
+import {withRouter} from "react-router-dom";
+import {withSnackbar} from "notistack";
+import {withStyles} from "@material-ui/core/styles";
+import {inject, observer} from "mobx-react";
+import {Box, Toolbar, Typography} from "@material-ui/core";
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import dayjs from 'dayjs';
+import {FormControl, FormControlLabel, FormLabel, Radio, RadioGroup} from "@mui/material";
+
+const styles = theme => ({
+    mainContainer : {
+        flexGrow : 1,
+        padding: theme.spacing(3)
+    },
+    appBarSpacer : theme.mixins.toolbar,
+    mainContent: {
+        marginTop: theme.spacing(2),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    toolbar: {
+        width: '100%',
+    },
+    contentBox:{
+        width: '100%',
+        background: 'white',
+        marginTop: 20,
+        paddingTop: 10,
+        paddingBottom: 40,
+        boxSizing: 'border-box',
+        boxShadow: '0 2px 4px 0 rgba(0, 0, 0, 0.25)'
+    },
+    contentPadding:{
+        width: '100%',
+        padding: '0 30px',
+        boxSizing: 'border-box',
+        backgroundColor: 'white',
+    },
+    labelText: {
+        fontSize: '0.938rem',
+        color: 'rgba(102, 102, 102, 0.8)',
+        fontWeight: 500,
+        marginRight: 0,
+        width: 80,
+    },
+    lineText: {
+        justifyContent: 'flex',
+        paddingTop: 3,
+        alignItems: 'center',
+        display: 'flex',
+    }
+})
+
+class MyPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            second : dayjs().format("YYYY-MM-DD"),
+            // renameSwitch : true,
+            // realUpdateTime : this.props.authStore.loginUser.updatedDatetime,
+            useName: this.props.authStore.loginUser.enabled ? "활성화" : "비활성화",
+        }
+    }
+
+    // CanChangeInfo = () => {
+    //     this.setState({
+    //         renameSwitch : false
+    //     })
+    // }
+
+    // EndChangeInfo = () => {
+    //     let nowTime = Date();
+    //     this.setState({
+    //         renameSwitch : true,
+    //         realUpdateTime: nowTime,
+    //     })
+    // }
+
+    componentDidMount() {
+        this.props.authStore.convertStatus()
+    }
+
+    // componentDidMount() {
+    //     this.props.authStore.startChangeInfo()
+    // }
+
+    // handleEnabled = (e) => {
+    //     this.setState({
+    //         useName : e.target.value
+    //     })
+    // }
+
+    // componentWillUnmount() {
+    //     this.setState({
+    //         realEnabled : this.props.authStore.loginUser.enabled
+    //     })
+    // }
+
+    render() {
+        const { classes, authStore } = this.props;
+        // console.log(authStore.realUpdateTime, authStore.loginUser)
+        console.log(authStore.loginUser.enabled)
+
+        return (
+            <div className={classes.mainContainer}>
+                <div className={classes.appBarSpacer}>
+                    <Toolbar className={classes.mainContent} />
+
+                    <Typography variant="h4" component="h2">
+                        마이 페이지
+                    </Typography>
+                    <Typography>{this.state.second}</Typography>
+
+                    <Box className={classes.contentBox}>
+                        <Box className={classes.contentPadding}>
+                            <Box className={classes.lineText}>
+                                <Stack sx={{ alignItems : 'center' }} >
+                                    <TextField
+                                        id="id"
+                                        label="ID"
+                                        defaultValue={authStore.loginUser.id}
+                                        InputLabelProps={
+                                            {shrink: true}
+                                        }
+                                        inputProps={
+                                            {readOnly: authStore.renameSwitch}
+                                        }
+                                        sx={
+                                            {m:2}
+                                        }
+                                        onChange={(e)=>authStore.handleOnChangeId(e.target.value)}
+                                    />
+                                    {authStore.renameSwitch ? "" : <span style={{color : "green"}}>ID 변경 가능합니다.</span>}
+                                    <TextField
+                                        id="name"
+                                        label="이름"
+                                        defaultValue={authStore.loginUser.name}
+                                        InputLabelProps={
+                                            {shrink: true}
+                                        }
+                                        inputProps={
+                                            {readOnly: authStore.renameSwitch}
+                                        }
+                                        sx={
+                                            {m:2}
+                                        }
+                                        onChange={(e)=>authStore.handleOnChangeName(e.target.value)}
+                                    />
+                                    {authStore.renameSwitch ? "" : <span style={{color : "green"}}>이름 변경 가능합니다.</span>}
+                                    <TextField
+                                        id="type"
+                                        label="권한"
+                                        value={authStore.loginUser.type === "Admin" ? "관리자" : "일반회원"}
+                                        InputLabelProps={
+                                            {shrink: true}
+                                        }
+                                        inputProps={
+                                            {readOnly: true}
+                                        }
+                                        sx={
+                                            {m:2}
+                                        }
+                                    />
+                                    <TextField
+                                        id="first-signIn"
+                                        label="계정 생성일"
+                                        value={dayjs(authStore.loginUser.createdDatetime).format("YYYY.MM.DD")}
+                                        InputLabelProps={
+                                            {shrink: true}
+                                        }
+                                        inputProps={
+                                            {readOnly: true}
+                                        }
+                                        sx={
+                                            {m:2}
+                                        }
+                                    />
+                                    <TextField
+                                        id="renewal-info"
+                                        label="정보 갱신 날짜"
+                                        value={dayjs(authStore.loginUser.updatedDatetime).format("YYYY.MM.DD")}
+                                        InputLabelProps={
+                                            {shrink: true}
+                                        }
+                                        inputProps={
+                                            {readOnly: true}
+                                        }
+                                        sx={
+                                            {m:2}
+                                        }
+                                    />
+                                    {authStore.renameSwitch
+                                        ? <TextField
+                                            id="status"
+                                            label="계정 상태"
+                                            value={authStore.realStatus}
+                                            // value={this.state.useName}
+                                            InputLabelProps={
+                                                {shrink: true}
+                                            }
+                                            inputProps={
+                                                {readOnly: true}
+                                            }
+                                            sx={
+                                                {m:2}
+                                            }
+                                        />
+                                        :
+                                        <FormControl>
+                                            <FormLabel>계정 상태</FormLabel>
+                                            <RadioGroup
+                                                row
+                                                // value={"활성화"}
+                                                value={authStore.realStatus}
+                                                // value={this.state.useName}
+                                                onChange={(e)=>authStore.handleOnChangeStatus(e)}
+                                                // onChange={(e)=>authStore.handleOnChangeStatus(e.target.value)}
+                                                // onChange={(e)=>this.handleEnabled(e)}
+                                            >
+                                                <FormControlLabel control={<Radio/>} label={"활성화"} value={"활성화"} />
+                                                <FormControlLabel control={<Radio/>} label={"비활성화"} value={"비활성화"} />
+                                                {/*<FormControlLabel control={<Radio/>} label={"활성화"} value={true} />*/}
+                                                {/*<FormControlLabel control={<Radio/>} label={"비활성화"} value={false} />*/}
+                                            </RadioGroup>
+                                        </FormControl>
+                                    }
+                                    {authStore.renameSwitch
+                                        ? <button onClick={authStore.CanChangeInfo}>정보 수정</button>
+                                        : <button onClick={authStore.EndChangeInfo}>수정 완료</button>}
+                                </Stack>
+                            </Box>
+                        </Box>
+                    </Box>
+                </div>
+            </div>
+        )
+
+    }
+
+}
+
+export default withSnackbar(withRouter((withStyles(styles) (
+    inject('authStore')(
+        observer(MyPage)
+    )
+))));
