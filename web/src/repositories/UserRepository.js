7ba6dc65a1cb34a43f6cc5896ba619b2799740e9
bckim-onthelive <dntldn@naver.com>
2022-11-03 16:50:14 +0900

    repository 내 api 주소 변경(JH)

--- web/src/repositories/UserRepository.js
+++ web/src/repositories/UserRepository.js
@@ -5,7 +5,7 @@ export default class UserRepository extends Repository {
     constructor(props) {
         super();
 
-        this.requestPrefix = props.serverContextPath + "/api/v1/userInfo";
+        this.requestPrefix = props.serverContextPath + "/api/v1/user";
     }
 
 
